a656b673906151d2bc7108cabcf88a8ffb32f716
Jack Maloney <jmmaloney4@gmail.com>
2013-08-26 21:16:29 -0500

    Pushing WIP, with sorting the information

--- jmm/ml/arff/AlgorithmManager.java
+++ jmm/ml/arff/AlgorithmManager.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 import jmm.ml.data.Data;
 
 public class AlgorithmManager {
-	
+
 	public static Data BigArrayObj;
 	static int attrs;
-	static float[] attribute;
-	static float[] attributeRanges;
-	
+
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -18,13 +16,13 @@ public class AlgorithmManager {
 	public static void main(String[] args) throws IOException {
 		BigArrayObj = ArffFileReader.fileReader.load(); 
 		attrs = ArffFileReader.fileReader.getNumAttrs();
-		attributeRanges = new float[attrs * 2];
-		attribute = new float[BigArrayObj.getLength() / attrs];
-		
-		for (int k = 0; k <= (attribute.length); k++) {
-			
-		}
-		
+
+	}
+
+	private float[] getColumn(int c) {
+		float[] ra = new float[BigArrayObj.getLength() / attrs];
+		BigArrayObj.get(loc)
+		return ra;
 	}
 
 }
---
+++ jmm/ml/dtree/IDecisionTree.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package jmm.ml.dtree;
+
+/**
+ * @author john
+ *
+ */
+public interface IDecisionTree {
+	
+	public IDTNode getRoot();
+	
+	
+}
